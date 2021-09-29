2c9f31ded4d53fcf638ccd7f28637aab096461fb
yoons <alohalo98@naver.com>
2021-09-30 02:37:02 +0900

    카운터 버튼들 태그이름으로 찾아서도 해보기

--- 8주차/js 실습/HTML과 JS 연동/카운터.js
+++ 8주차/js 실습/HTML과 JS 연동/카운터.js
@@ -1,6 +1,13 @@
+// Id로 찾기
 const number = document.getElementById('number');
-const increase = document.getElementById('increase');
-const decrease = document.getElementById('decrease');
+// const increase = document.getElementById('increase');
+// const decrease = document.getElementById('decrease');
+
+// 태그 이름으로 찾기
+const buttons = document.querySelectorAll('button');
+const [increase, decrease] = buttons; // buttons에서 increase와 decrease로 추출 
+
+
 
 increase.onclick = () => {
     const current = parseInt(number.innerText, 10); 
