75903b51ccd227c21656148792dd2ac848aa5333
yoons <alohalo98@naver.com>
2021-10-10 22:12:13 +0900

    커밋 확인

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled/*, {css}*/ , {ThemeProvider} from "styled-components";
+import Button from "./components/Button";
 
+// Circle이라는 styled-component 생성
+// const Circle = styled.div`
+//   width : 5rem;
+//   height : 5rem;
+//   background : ${props => props.color};
+//   border-radius : 50%;
+//   ${props => props.huge && css`
+//     width : 10rem;
+//     height : 10rem;
+//   `}
+// `;
+
+const AppBlock = styled.div`
+  width:512px;
+  margin : 0 auto;
+  margin-top: 4rem;
+  border : 1px solid black;
+  padding : 1rem;
+
+`;
+
+const ButtonGroup = styled.div`
+  & + &{
+    margin-top:1rem;
+  }
+  
+  
+`;
+
+const palette = {
+  blue : '#228be6',
+  gray: '#496057',
+  pink : '#f06595'
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Circle color="blue" huge />
+    <ThemeProvider
+      theme = {{
+        palette
+      }}>
+      <AppBlock>
+         <ButtonGroup>
+          <Button color = 'pink' size = 'large'>BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size = 'small'color = 'gray'>BUTTON</Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button color = 'pink' size = 'large' outline>BUTTON</Button>
+          <Button outline>BUTTON</Button>
+          <Button size = 'small'color = 'gray' outline>BUTTON</Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button size = 'large' fullWidth>BUTTON</Button>
+          <Button size = 'large' color = 'gray' fullWidth>BUTTON</Button>
+          <Button color = 'pink' size = 'large' fullWidth>BUTTON</Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Dialog.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+
+const DarkBackground = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.8);
+`;
+
+
+// 흰색박스
+const DialogBlock = styled.div`
+  width: 320px;
+  padding: 1.5rem;
+  background: white;
+  border-radius: 2px;
+  h3 {
+    margin: 0;
+    font-size: 1.5rem;
+  }
+  p {
+    font-size: 1.125rem;
+  }
+`;
+
+const ButtonGroup = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const ShortMarginButton = styled(Button)`
+  & + & {
+    margin-left: 0.5rem;
+  }
+`;
+
+function Dialog() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Dialog;
