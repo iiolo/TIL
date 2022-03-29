f83502e65ddd265cd5bd75270e53c80afca6554e
Soyun Kim <89250252+iiolo@users.noreply.github.com>
2022-03-29 09:57:35 +0900

    Add files via upload

---
+++ day0329/CommentTest.java
@@ -0,0 +1,22 @@
+package day0329;
+
+public class CommentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 주석문에 대한 실습입니다.
+		/*
+		 주석이란 프로그램 실행에 영향을 끼치지 않고 설명을 이한 문장을 말합니다.
+		 한 줄 주석으 달 수도 있고 여러 줄에 걸쳐서 주석을 달 수도 있습니다.
+		 */
+		
+		int a = 10;
+		int b;
+		b =5;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+b);
+		
+	}
+
+}
